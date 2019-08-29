454f50221d9b11e0064c2bf836b410462a1baf1f
Zefeng Zhang <zefeng.zhang@dxc.com>
2019-08-29 09:15:07 +0800

    init

---
+++ src/main/java/com/demo/oldwjavaexecution/OldwJavaExecutionApplication.java
@@ -0,0 +1,13 @@
+package com.demo.oldwjavaexecution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldwJavaExecutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldwJavaExecutionApplication.class, args);
+	}
+
+}
